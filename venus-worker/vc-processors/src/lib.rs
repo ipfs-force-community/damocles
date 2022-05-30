9dd9d8422c34a817a39477df725e69598cbaef45
dtynn <dtynn@163.com>
2022-05-30 13:31:58 +0800

    chore(vc-processors): init crate

---
+++ venus-worker/vc-processors/src/lib.rs
@@ -0,0 +1,7 @@
+#![warn(missing_docs)]
+//! vc-processors contains the types and builtin external processors for
+//! [venus-cluster/venus-worker](https://github.com/ipfs-force-community/venus-cluster/tree/main/venus-worker).
+//!
+//! This crate aims at providing an easy-to-use, buttery-inlcuded framework for third-party
+//! developers to implement customized external processors.
+//!
