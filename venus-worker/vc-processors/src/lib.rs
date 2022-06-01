3f8cff8804f8de5e7c79064e7f99342e51abdc30
dtynn <dtynn@163.com>
2022-06-01 18:22:09 +0800

    docs(vc-processors): descriptions for usages

--- venus-worker/vc-processors/src/lib.rs
+++ venus-worker/vc-processors/src/lib.rs
@@ -5,6 +5,10 @@
 //! This crate aims at providing an easy-to-use, buttery-inlcuded framework for third-party
 //! developers to implement customized external processors.
 //!
+//! This crate could be used to:
+//! 1. interact with builtin external processors in venus-worker.
+//! 2. wrap close-source processors for builtin tasks in venus-worker.
+//! 3. implement any customized external processors for other usecases.
 
 pub mod core;
 pub(crate) mod sys;
