860f531f5fd84338ac0600bc44b0b702cc0bbf1e
0x5459 <0x5459@protonmail.com>
2022-06-13 09:19:20 +0800

    refactor(hwinfo): remove useless code

--- venus-worker/src/bin/venus-worker/main.rs
+++ venus-worker/src/bin/venus-worker/main.rs
@@ -5,7 +5,6 @@ use tokio::runtime::Builder;
 use venus_worker::{logging, start_deamon};
 
 mod generator;
-mod hwinfo;
 mod processor;
 mod store;
 mod worker;
@@ -34,7 +33,6 @@ pub fn main() -> Result<()> {
     let processor_cmd = processor::subcommand();
     let store_cmd = store::subcommand();
     let worker_cmd = worker::subcommand();
-    let hwinfo_cmd = hwinfo::subcommand();
 
     let ver_string = format!("v{}-{}", env!("CARGO_PKG_VERSION"), env!("GIT_COMMIT"));
 
@@ -45,8 +43,7 @@ pub fn main() -> Result<()> {
         .subcommand(generator_cmd)
         .subcommand(processor_cmd)
         .subcommand(store_cmd)
-        .subcommand(worker_cmd)
-        .subcommand(hwinfo_cmd);
+        .subcommand(worker_cmd);
 
     let matches = app.get_matches();
 
@@ -64,8 +61,6 @@ pub fn main() -> Result<()> {
         (store::SUB_CMD_NAME, Some(args)) => store::submatch(args),
 
         (worker::SUB_CMD_NAME, Some(args)) => worker::submatch(args),
-
-        (hwinfo::SUB_CMD_NAME, Some(args)) => hwinfo::submatch(args),
 
         (name, _) => Err(anyhow!("unexpected subcommand `{}`", name)),
     }
