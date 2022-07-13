55bdf5b820e16e8a5f273de174ae89fa26ac9354
dtynn <dtynn@163.com>
2022-07-13 16:20:25 +0800

    feat(vsmgr): introduce lotus-miner api

---
+++ venus-sector-manager/pkg/lotusminer/lotus_miner.go
@@ -0,0 +1,29 @@
+package lotusminer
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/filecoin-project/go-jsonrpc"
+	"github.com/filecoin-project/lotus/api"
+	vapi "github.com/filecoin-project/venus/venus-shared/api"
+)
+
+func New(ctx context.Context, addr string, token string) (StorageMiner, jsonrpc.ClientCloser, error) {
+	ainfo := vapi.NewAPIInfo(addr, token)
+
+	var a api.StorageMinerStruct
+	apiAddr, err := ainfo.DialArgs(vapi.VerString(0))
+	if err != nil {
+		return nil, nil, fmt.Errorf("get api addr: %w", err)
+	}
+
+	closer, err := jsonrpc.NewMergeClient(ctx, apiAddr, "Filecoin", vapi.GetInternalStructs(&a), ainfo.AuthHeader(), jsonrpc.WithRetry(true))
+	if err != nil {
+		return nil, nil, fmt.Errorf("dial: %w", err)
+	}
+
+	return &a, closer, nil
+}
+
+type StorageMiner = api.StorageMiner
