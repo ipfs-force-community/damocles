c24914c69c1e7dbe5f840437e303e4359d4f7b1f
dtynn <dtynn@163.com>
2022-08-17 15:33:04 +0800

    fix(vworker): allowe_xxxx usages in rebuild planner

--- venus-worker/src/sealing/worker/task/planner/rebuild.rs
+++ venus-worker/src/sealing/worker/task/planner/rebuild.rs
@@ -124,8 +124,8 @@ impl<'c, 't> Rebuild<'c, 't> {
             self.task.ctx.global.rpc,
             allocate_rebuild_sector,
             AllocateSectorSpec {
-                allowed_miners: self.task.store.allowed_miners.as_ref().cloned(),
-                allowed_proof_types: self.task.store.allowed_proof_types.as_ref().cloned(),
+                allowed_miners: Some(self.task.store.config.allowed_miners.clone()),
+                allowed_proof_types: Some(self.task.store.config.allowed_proof_types.clone()),
             },
         };
 
