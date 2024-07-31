1cbfc0bfc451cc10c7f381b1acd3d2bb8d0179a4
simlecode <69969590+simlecode@users.noreply.github.com>
2024-07-31 15:59:54 +0800

    fix

--- damocles-worker/src/sealing/sealing_thread/planner/niporep.rs
+++ damocles-worker/src/sealing/sealing_thread/planner/niporep.rs
@@ -216,8 +216,7 @@ impl<'t> NiPoRep<'t> {
 
     fn handle_pc1_done(&self) -> Result<Event, Failure> {
         let verify_after_pc2 = self.task.sealing_ctrl.config().verify_after_pc2;
-        common::pre_commit2(self.task, verify_after_pc2)
-            .map(Event::PC2)
+        common::pre_commit2(self.task, verify_after_pc2).map(Event::PC2)
     }
 
     fn handle_pc2_done(&self) -> Result<Event, Failure> {
